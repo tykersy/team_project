62b25ae6c1738bbad107be493a5b4984437c6345
911carpediem <carpediem9011@gmail.com>
2026-05-28 16:21:29 +0900

    공지사항

--- src/main/java/com/kh/project/vo/BoardVO.java
+++ src/main/java/com/kh/project/vo/BoardVO.java
@@ -1,18 +1,16 @@
 package com.kh.project.vo;
 
-import org.apache.ibatis.type.Alias;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Alias("board")
-public class BoardVO {
-    
-    private int idx, sabun;
-    private String title, content, file, created, updated;
 
+public class BoardVO {
+    private int idx, sabun;
+    private String saname, title, content, file, created, updated;
 }
