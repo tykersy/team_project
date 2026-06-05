73c70e50dc4a2d0ed5bc3a63d8133f2d25d5d645
HYEJI-1216 <rosemary4132@naver.com>
2026-06-05 17:14:15 +0900

    사원 정보 관리  삭제, 수정 오류수정+ 유효성체크+에러메세지 창 수정

---
+++ src/main/java/com/kh/project/controller/JobPositionController.java
@@ -0,0 +1,11 @@
+package com.kh.project.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class JobPositionController {
+    
+}
